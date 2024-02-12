3fbf88d74f5ad1aa6e494b251fde5629cf99a762
Ebin Shaji <39409167+ebinshaji@users.noreply.github.com>
2024-02-13 04:52:49 +0530

    new update

--- Script/Script.js
+++ Script/Script.js
@@ -1,3 +1,5 @@
+//for header
+
 const menuToggle= document.querySelector(".menu-bars");
 const nav = document.querySelector("nav ul");
 
@@ -5,3 +7,26 @@ const nav = document.querySelector("nav ul");
 menuToggle.addEventListener("click", () => {
   nav.classList.toggle("slide");
   });
+
+  window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    var scrollPosition = window.scrollY;
+
+    // Change background color based on device width
+    if (window.innerWidth > 1024) {
+        // For larger screens
+        if (scrollPosition > 700) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    } else {
+        // For mobile devices
+        if (scrollPosition > 100) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    }
+});
+//write code below
